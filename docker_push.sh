ded8a3d64ff6efa66743a1c1c2a1e6c8760e3501
lexstar1 <59297711+lexstar1@users.noreply.github.com>
2021-03-11 00:29:48 -0500

    modified .travis.yml and verification.sh

--- docker_push.sh
+++ docker_push.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker tag challenge_project_web:latest enochnooli/challenge_project_web
-docker push enochnooli/challenge_project_web
+docker tag challenge_project_web:latest enochnooli/challenge_project_web:latest
+docker push enochnooli/challenge_project_web:latest``
